b51aecdbf8ff75f1a4667b9efa68bd043d09d121
Vanja Sretenović <31417903+vanja032@users.noreply.github.com>
2022-10-22 20:10:08 +0200

    Update FetchUrls.js

--- FetchUrls.js
+++ FetchUrls.js
@@ -1,6 +1,6 @@
-var jq = document.createElement("script");
-jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js";
-document.getElementsByTagName("head")[0].appendChild(jq);
+var jquery = document.createElement("script");
+jquery.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js";
+document.getElementsByTagName("head")[0].appendChild(jquery);
 
 
 $(document).ready(function(){
