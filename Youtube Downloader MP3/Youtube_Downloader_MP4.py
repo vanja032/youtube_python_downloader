38b0c074211debd642e6f0f786372f82cd58e335
Vanja Sretenović <31417903+vanja032@users.noreply.github.com>
2022-10-22 20:07:25 +0200

    Create Youtube_Downloader_MP4.py

---
+++ Youtube Downloader MP3/Youtube_Downloader_MP4.py
@@ -0,0 +1,23 @@
+from pytube import YouTube
+import os
+from os.path import exists
+
+print("Youtube MP4 downloader")
+file = open("urls.txt", "r")
+urls = file.read().split("\n")
+format = ".mp4"
+destination = "<destination path>"
+
+for url in urls:
+    try:
+        print(url)
+        yt = YouTube(url)
+        print(yt.title)
+        if not exists(destination + "/" + yt.title + format):
+            video = yt.streams.get_highest_resolution()
+            out_file = video.download(output_path=destination)
+            print("Finished")
+        else:
+            print("File exist!")
+    except:
+        continue
