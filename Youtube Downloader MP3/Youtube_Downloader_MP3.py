cc13c9ec5ab82018ad3c24a6d58732a5ce7b7a29
Vanja Sretenović <31417903+vanja032@users.noreply.github.com>
2022-10-22 20:06:29 +0200

    Update Youtube_Downloader_MP3.py

--- Youtube Downloader MP3/Youtube_Downloader_MP3.py
+++ Youtube Downloader MP3/Youtube_Downloader_MP3.py
@@ -1,18 +1,26 @@
 from pytube import YouTube
 import os
+from os.path import exists
 
+print("Youtube MP3 downloader")
 file = open("urls.txt", "r")
 urls = file.read().split("\n")
+format = ".mp3"
+destination = "<destination path>"
+
 for url in urls:
     try:
         print(url)
         yt = YouTube(url)
-        video = yt.streams.filter(only_audio=True).first()
-        destination = "<destination path>"
-        out_file = video.download(output_path=destination)
-        base, ext = os.path.splitext(out_file)
-        new_file = base + '.mp3'
-        os.rename(out_file, new_file)
-        print("Finished")
+        print(yt.title)
+        if not exists(destination + "/" + yt.title + format):
+            video = yt.streams.filter(only_audio=True).first()
+            out_file = video.download(output_path=destination)
+            base, ext = os.path.splitext(out_file)
+            new_file = base + format
+            os.rename(out_file, new_file)
+            print("Finished")
+        else:
+            print("File exist!")
     except:
         continue
