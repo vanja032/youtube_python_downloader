3b7a7ba8f0831b0bb06e41f6659d7bfab89defda
Vanja Sretenović <31417903+vanja032@users.noreply.github.com>
2022-10-22 19:25:07 +0200

    Add files via upload

---
+++ FetchUrls.js
@@ -0,0 +1,21 @@
+var jq = document.createElement("script");
+jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js";
+document.getElementsByTagName("head")[0].appendChild(jq);
+
+
+$(document).ready(function(){
+    var content = "";
+    $("#items ytd-playlist-panel-video-renderer").each(function(i, obj){
+        content += "https://www.youtube.com" + $(this).children("a").attr("href") + "\n";
+    });
+    console.log(content);
+});
+
+
+
+$(document).ready(function(){
+    $("#items ytd-playlist-panel-video-renderer").each(function(i, obj){
+        var element = "<iframe class='dwns' src='" + "https://www.youtube.com" + $(this).children("a").attr("href") + "' width='100%' height='100%' allowtransparency='true' scrolling='no' style='border:none'></iframe>";
+        $("#primary").append(element);
+    });
+});
---
+++ Youtube Downloader MP3/Youtube_Downloader_MP3.py
@@ -0,0 +1,15 @@
+from pytube import YouTube
+import os
+
+file = open("urls.txt", "r")
+urls = file.read().split("\n")
+for url in urls:
+    print(url)
+    yt = YouTube(url)
+    video = yt.streams.filter(only_audio=True).first()
+    destination = "C:/Users/Vanja Sretenovic/Desktop/NovePesme"
+    out_file = video.download(output_path=destination)
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+    print("Finished")
